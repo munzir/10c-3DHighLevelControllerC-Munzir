5fac7ca34508efc650db5d96c90b17b0f4d1b039
Shimin Zhang <john.shimin.zhang@gmail.com>
2018-04-28 13:35:24 -0400

    chage names to 3d, remove a few cache files from repo

--- build/examples/krang2d/CMakeFiles/Krang2D_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Krang2D_automoc.cpp"
-  "CMakeFiles/Krang2D_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Krang2D_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
